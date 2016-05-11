7fe4cfbde3ef1b48482aefcb30d476fe52d0312f
Coding <Liu370890676@163.com>
2016-05-11 10:59:39 +0800

    Site updated: 2016-05-11 10:59:38

--- js/embed.js
+++ js/embed.js
@@ -16,7 +16,7 @@ function checkMobile() {
 //管理员判断开始
 function sskadmin(e) {
     var ssk = '';
-    if (e.user_id == 6283242258208654082) {
+    if (e.user_id == 6283259863527064321) {
         if (checkMobile()) {
             ssk = '<span class="ua"><span class="sskadmin">博主</span></span><br><br>';
         } else {
